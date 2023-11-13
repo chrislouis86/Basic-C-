b8f5c22cb2800a536138cf642b6fd32849cda459
Christopher Morgan <60473703+chrislouis86@users.noreply.github.com>
2023-11-13 17:38:08 +0000

    Add files via upload
    This assessment will operate as a "seen exam", operated in an open book test format, so all course materials and use of the internet to search for advice will be allowed. Students will be provided with a worksheet on Monday 8th March, and asked to complete and submit the text exam by 15:00 on Thursday 11th March. A series of five programming tasks will be assigned within the class test. This is designed to replicate a basic programming competency test, as would be expected in a job interview.
    Assessment Criteria:
    
    Ability to write a C++ function to a given specification.
    
    Demonstrate an understanding of basic programming concepts, and when are appropriate to use.
    
    Ability to create legible and well presented code.
    
    Ability to follow the brief and completeness of answers

---
+++ classTest.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <array>
+#include <cmath>
+#include <climits>
+
+
+// QUESTION ONE
+//code for question one here
+
+// QUESTION TWO
+//code for question two here
+
+// QUESTION THREE
+//code for question three here
+
+// QUESTION FOUR
+//code for question four here
+
+// QUESTION FIVE
+//code for question FIVE here
+
+// QUESTION SIX
+//code for question SIX here
+
+// QUESTION SEVEN
+//code for question seven here
+
+// QUESTION EIGHT
+void FizzBuzz()
+{
+//code for question eight here
+}
+
+
+
+
+int main() {
+
+
+// For Question 6
+    std::array<float,8> testData = {-0.9, 0.8, -0.6, 0.4, 0.1, 0.6, 0.2, 0.4};
+
+    return 0;
+}
